3942d7e4f14cabb2a5c129209e86d46025a619d6
Josiah Morris <josiahbmorris@hotmail.com>
2023-04-28 02:30:44 +0900

    Added root variable, and buildTree function to tree factory

--- binary_search_tree.js
+++ binary_search_tree.js
@@ -7,6 +7,25 @@ const node = (data) => {
 }
 
 const tree = (array) => {
+  const buildTree = (array, start, end) => {
+    const sorted = array.sort((a, b) => a - b).filter((item, i) => array.indexOf(item) === i)
+
+    const buildBranch = (start, end) => {
+      if (start > end) { return null }
+
+      const mid = Math.ceil((start + end) / 2)
+      const treeRoot = node(sorted[mid])
+
+      treeRoot.left = buildBranch(start, mid - 1)
+      treeRoot.right = buildBranch(mid + 1, end)
+
+      return treeRoot
+    }
+    return buildBranch(0, sorted.length - 1)
+  }
+
+  const root = buildTree(array, 0, array.lengh - 1)
+
   const prettyPrint = (node, prefix = '', isLeft = true) => {
     if (node === null) { return }
     if (node.right !== null) {
@@ -17,6 +36,7 @@ const tree = (array) => {
       prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true)
     }
   }
+  prettyPrint(root)
 }
 
 const thing = [1, 2, 3, 4, 5, 6, 7, 8, 9]
